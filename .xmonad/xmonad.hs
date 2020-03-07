5d00a5078b80a5e3848c2fd189ff79a6978bfbb0
rehajel <rehajel@gmail.com>
2020-03-07 08:27:44 +0100

    commit comment

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,6 +6,7 @@ import XMonad.Actions.SpawnOn
 import XMonad.Util.SpawnOnce
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Grid
+import XMonad.Layout.ThreeColumns
 import XMonad.Hooks.InsertPosition
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
@@ -17,6 +18,7 @@ myBorderWidth = 1
 myModMask = mod4Mask
 myTerminal = "alacritty"
 cmus = myTerminal ++ " --title cmus -e cmus"
+neomutt = myTerminal ++ " --title neomutt -e neomutt"
 newsboat = myTerminal ++ " --title newsboat -e newsboat"
 pulsemixer = myTerminal ++ " --title pulsemixer -e pulsemixer"
 myLauncher = "/usr/bin/rofi -show run"
@@ -27,29 +29,30 @@ myClickJustFocuses = False
 ---- APP RULES
 myManageHook = composeAll [
     --className =? "qutebrowser"      --> doShift "2",
-    title     =? "cmus"             --> doShift "0",
+    --title     =? "cmus"             --> doShift "0",
     className =? "discord"          --> doShift "9",
     --className =? "Bitwarden"        --> doShift "8",
     className =? "Yad"              --> doFloat,
     title     =? "pulsemixer"       --> doFloat,
     className =? "Thunar"           --> doFloat,
-    insertPosition Below Newer]
+    insertPosition Above Newer
+    ]
 
 ---- STARTUP
 myStartupHook = do
     setWMName "LG3D"
     -- Focus the second screen.
-    screenWorkspace 1 >>= flip whenJust (windows . W.view)
+    --screenWorkspace 1 >>= flip whenJust (windows . W.view)
     -- Force the second screen to "9", e.g. if the first screen already has
     -- the workspace associated the screens will swap workspaces.
-    windows $ W.greedyView "9"
+    --windows $ W.greedyView "9"
     -- Focus the first screen again.
-    screenWorkspace 0 >>= flip whenJust (windows . W.view)
+    --screenWorkspace 0 >>= flip whenJust (windows . W.view)
     -- Launch Startup Stuff
     spawnOnOnce "9" "discord"
     spawnOnOnce "8" "bitwarden"
     spawnOnOnce "9" "telegram-desktop"
-    spawn "feh --bg-scale ~/Wallpapers/Climbing/MoonLoveComic.png"
+    spawn "feh --bg-center ~/Wallpapers/Ultrawide/wallpapers_hatschl/Oriental_2.jpg"
     spawnOnce "picom -bc --config ~/.config/picom.conf"
     spawnOnce "unclutter &"
     spawnOnce "dunst &"
@@ -64,8 +67,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
       ((modMask .|. shiftMask,      xK_r),              restart "xmonad" True),
       -- Move Focus
       ((modMask,                    xK_space),          windows W.focusMaster),
-      ((modMask,                    xK_j),              windows W.focusDown),
-      ((modMask,                    xK_k),              windows W.focusUp),
+      ((modMask,                    xK_j),              windows W.focusUp),
+      ((modMask,                    xK_k),              windows W.focusDown),
       -- Swap Monitors
       ((modMask,                    xK_w),              swapNextScreen),
       -- Focus Monitors
@@ -73,8 +76,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
       -- Move Window to next Screen
       ((modMask .|. shiftMask,      xK_q),              shiftNextScreen >> nextScreen),
       -- Move Windows in Layout
-      ((modMask .|. shiftMask,      xK_j),              windows W.swapDown),
-      ((modMask .|. shiftMask,      xK_k),              windows W.swapUp),
+      ((modMask .|. shiftMask,      xK_j),              windows W.swapUp),
+      ((modMask .|. shiftMask,      xK_k),              windows W.swapDown),
       ((modMask .|. shiftMask,      xK_space),         windows W.swapMaster),
       -- Change Size
       ((modMask,                    xK_l),              sendMessage Expand),
@@ -87,10 +90,11 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
       ((modMask .|. shiftMask,      xK_x),              kill),
       --Spawn Apps
       ((modMask,                    xK_d),              spawnHere "/usr/bin/rofi -show run"),
-      ((modMask,                    xK_i),              spawnHere "qutebrowser"),
+      ((modMask,                    xK_i),              spawnHere "firefox"),
       ((modMask,                    xK_m),              spawn cmus), 
       ((modMask,                    xK_Return),         spawnHere myTerminal),
-      ((modMask,                    xK_e),              spawnHere "thunar"),
+      ((modMask.|. shiftMask,       xK_e),              spawnHere "thunar"),
+      ((modMask,                    xK_e),              spawnHere neomutt),
       ((modMask .|. shiftMask,      xK_n),              spawnHere newsboat),
       ((modMask,                    xK_c),              spawnHere "~/.scripts/clock"),
       ((modMask .|. shiftMask,      xK_c),              spawnHere "~/.scripts/popupcalendar --popup"),
@@ -111,10 +115,10 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 --- LAYOUTS
 myLayout =  lessBorders Screen $ onWorkspace "5" Grid tiled ||| noBorders (Full)
     where
-        tiled = Tall nmaster delta ratio
+        tiled = ThreeColMid nmaster delta ratio
         nmaster = 1
-        ratio = 1/1.8
         delta = 3/100
+        ratio = 1/2
 
 ---- MAIN
 main = do
